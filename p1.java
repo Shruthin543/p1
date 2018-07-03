1ee843af5d4599907c7238522bc0592133366157
Shruthin543 <40788221+Shruthin543@users.noreply.github.com>
2018-07-03 16:10:47 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,17 @@
+package helloworld;
+import java.util.Scanner;
+public class p1 {
+	public static void main(String args[])
+	{
+		Scanner s =new Scanner(System.in);
+		int x=s.nextInt();
+		int s1=0;
+		while(x!=0) {
+			s1+=x;
+			x--;
+		}
+		System.out.println("result is"+ s1);
+	}
+
+}
+
