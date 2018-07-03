bee342eb77e83b151948269d30a924113b5a89fd
Shruthin543 <40788221+Shruthin543@users.noreply.github.com>
2018-07-03 15:45:32 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,35 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class p2 {
+	public static void main(String args[])
+	{
+		System.out.println("enter the input");
+		Scanner s = new Scanner (System.in);
+		int x= s.nextInt();
+		int y= s.nextInt();
+		System.out.println("enter the operation to be performed");
+		String oper= s.next();
+		switch(oper)
+		{
+		case "+": System.out.println("result is"+ (x+y));
+		break;
+		case "-": System.out.println("result is"+ (x-y));
+		break;
+		case "*": System.out.println("result is"+ (x*y));
+		break;
+	
+		case "/": 
+			if(y==0)
+				System.out.println("cannot perform");
+		else
+			System.out.println("result is"+ (x/y));
+		break;
+		default:
+			System.out.println("no match");
+		}
+		
+	}
+
+}
